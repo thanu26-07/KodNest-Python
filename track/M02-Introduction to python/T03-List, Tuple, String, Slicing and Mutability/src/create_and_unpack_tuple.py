2afd60f5f8001febdd997ba4b56775142fe22e12
thanu26-07 <thanujabhupalam@gmail.com>
2026-08-19 22:16:20 +0530

    create and unpack a student record

---
+++ track/M02-Introduction to python/T03-List, Tuple, String, Slicing and Mutability/src/create_and_unpack_tuple.py
@@ -0,0 +1,8 @@
+name=input()
+course=input()
+score=int(input())
+student_record=(name,course,score)
+un1=student_record[0]
+un2=student_record[1]
+un3=student_record[2]
+print(un1,un2,un3,sep='\n')
