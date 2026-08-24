287b4b55c773b9db31d06cd4f796487a2b4d9a96
thanu26-07 <thanujabhupalam@gmail.com>
2026-08-24 11:31:39 +0530

    update a tuple without modifing it

--- track/M02-Introduction to python/T03-List, Tuple, String, Slicing and Mutability/src/analyzer_list_scores.py
+++ track/M02-Introduction to python/T03-List, Tuple, String, Slicing and Mutability/src/analyzer_list_scores.py
@@ -8,8 +8,10 @@ for i in range(n):
 search_score=int(input())
 highest=max(scores)
 lowest=min(scores)
+
 print(highest)
 print(lowest)
+print(total)
 if search_score in scores:
     print("Found")
 else:
---
+++ track/M02-Introduction to python/T03-List, Tuple, String, Slicing and Mutability/src/update_a_tuple_without_modifing_it.py
@@ -0,0 +1,8 @@
+name=input()
+week=input()
+status=input()
+course_details=(name,week, status)
+updated=input()
+new=(name, updated, status)
+course_details=new
+print(course_details)
