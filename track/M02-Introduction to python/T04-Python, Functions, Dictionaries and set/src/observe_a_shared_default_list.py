93e88dc72eaa295d3bf57b2bf7c8ba84d3308c6c
thanu26-07 <thanujabhupalam@gmail.com>
2026-08-25 22:54:34 +0530

    observe_a_shared_default_list

---
+++ track/M02-Introduction to python/T04-Python, Functions, Dictionaries and set/src/observe_a_shared_default_list.py
@@ -0,0 +1,9 @@
+def add_student(name, students=[]):
+    students.append(name)
+    print(students)
+first=input()
+second=input()
+third=input()
+add_student(first)
+add_student(second)
+add_student(third)
