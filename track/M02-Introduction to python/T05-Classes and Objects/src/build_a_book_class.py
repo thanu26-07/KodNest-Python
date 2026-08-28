236811dc48b43999242477f86f3efd653a8faf78
thanu26-07 <thanujabhupalam@gmail.com>
2026-08-28 22:28:37 +0530

    build a book class

---
+++ track/M02-Introduction to python/T05-Classes and Objects/src/build_a_book_class.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self,title,author, price):
+        self.title=title
+        self.author=author
+        self.price=price
+title=input().strip()
+author=input().strip()
+price=int(input())
+book=Book(title,author,price)
+print("Book Details")
+print(f"Title:{book.title}")
+print(f"Author:{book.author}")
+print(f"Price:{book.price}")
