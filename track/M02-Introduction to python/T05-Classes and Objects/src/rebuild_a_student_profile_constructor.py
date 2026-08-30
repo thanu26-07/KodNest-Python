45e5377fcbbdd8516d6381b61048db0892dcfa81
thanu26-07 <thanujabhupalam@gmail.com>
2026-08-30 20:53:23 +0530

    rebuild a student profile constructor

---
+++ track/M02-Introduction to python/T05-Classes and Objects/src/rebuild_a_student_profile_constructor.py
@@ -0,0 +1,28 @@
+class StudentProfile:
+    def __init__(
+        self,
+        student_id,
+        name,
+        course,
+        experience,
+        skills
+    ):
+        # Store all received values as instance attributes
+        self.student_id = student_id
+        self.name = name
+        self.course = course
+        self.experience = experience
+        self.skills = skills
+
+# Read inputs from the user
+student_id = int(input().strip())
+name = input().strip()
+course = input().strip()
+experience = int(input().strip())
+skills = input().strip().split()
+
+# Create one StudentProfile object
+student1 = StudentProfile(student_id, name, course, experience, skills)
+
+# Print the data stored in the object
+print(f"Student ID: {student1.student_id}\nName: {student1.name}\nCourse: {student1.course}\nExperience in Years: {student1.experience}\nSkills: {', '.join(student1.skills)}")
